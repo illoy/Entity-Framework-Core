ce306b1bd9e0a476e7f7f6ebf2899ced1b04895c
illoy <illoy168@gmail.com>
2022-08-25 22:25:25 +0300

    Lab4

---
+++ HW_EF_4_Polishchuk/Task_1/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task_1.Models
+{
+    public class Country
+    {
+        [Column("Country_Id")]
+        public int Id { get; set; }
+        [Column("CountryName")]
+        public string Name { get; set; } = null!;
+
+        public List<Company> Companies { get; set; }
+        public Country()
+        {
+            Companies = new List<Company>();
+        }
+    }
+}
---
+++ HW_EF_4_Polishchuk/Task_2/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string ProductNum { get; set; } = null!;
+        public decimal Cost { get; set; } = 0;
+        public int Count { get; set; } = 0;
+        public DateTime SellStartDate { get; set; }
+
+    }
+}
